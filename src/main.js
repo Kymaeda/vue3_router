d75517718f792cd46654ba53c0e5b08d940e0727
Kymaeda <kyo.m.720@gmail.com>
2022-07-31 10:45:26 +0900

    setup vue-router
    1. install vue-router@4
    
    ```
    yarn add vue-router@4
    ```
    
    2. remove unnecessary descriptions
    
    3. create vue single file components under /pages
    
    4. define routes
    
    ```
    import { createRouter, createWebHashHistory } from 'vue-router';
    import YourComponent from './pages/YourComponent.vue';
    
    const routes = [
      {
        path: '/specified_path',
        component: YourComponent,
      },
    ];
    
    const router = createRouter({
      history: createWebHashHistory(),
      routes,
    });
    
    const app = createApp(App);
    app.use(router);
    ```
    
    5. load `RouterView` in main.js(top level js file)
    ```
    <template>
      <RouterView />
    </template>
    ```

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue'
+import HomePage from './pages/HomePage.vue';
+import AboutPage from './pages/AboutPage.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/about',
+    component: AboutPage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
